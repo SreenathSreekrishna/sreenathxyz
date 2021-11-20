a5ba6f257da3c8ee50a394ce59126a42f1ce1284
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2021-11-20 11:56:36 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app=Flask('__main__')
-@app.route('/')
-def index():
-  return render_template('index.html',name='name')
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
