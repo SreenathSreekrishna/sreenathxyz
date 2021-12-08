f8c44fb7346a9b955c6be14b4bd99dd11eca4225
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2021-12-08 13:25:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,3 +27,43 @@ $("#other").click(function(){
   $(".otcontent").show();
   $(".accontent").hide();
 })
+var lazer= true;
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+function lineDistance(x, y, x0, y0){
+    return Math.sqrt((x -= x0) * x + (y -= y0) * y);
+};
+
+function drawLine(a, b, line) {
+  var pointA = $(a).offset();
+  var pointB = $(b).offset();
+  var pointAcenterX = $(a).width() / 2;
+  var pointAcenterY = $(a).height() / 2;
+  var pointBcenterX = $(b).width() / 2;
+  var pointBcenterY = $(b).height() / 2;
+  var angle = Math.atan2(pointB.top - pointA.top, pointB.left - pointA.left) * 180 / Math.PI;
+  var distance = lineDistance(pointA.left, pointA.top, pointB.left, pointB.top);
+  $(line).css('transform', 'rotate(' + angle + 'deg)');
+  $(line).css('width', distance + 'px');
+  $(line).css('position', 'absolute');
+  if(pointB.left < pointA.left) {
+    $(line).offset({top: pointA.top + pointAcenterY, left: pointB.left + pointBcenterX});
+  } else {
+    $(line).offset({top: pointA.top + pointAcenterY, left: pointA.left + pointAcenterX});
+  }
+}
+jQuery(document).ready(function(){
+   $('html').mousemove(function(e){
+     if (!lazer){
+       $(".line,.line2").hide()
+     }else{
+       $(".line,.line2").show()
+     }
+     var X = (e.pageX);
+     var Y = (e.pageY-10);
+     $(".b").css({'margin-top':Y,'margin-left':X})
+     drawLine('.a', '.b', '.line');
+     drawLine('.c', '.b', '.line2');
+   });
+});
